2df08e2a57a315b33285997e8efbe4b6700f2d50
Oliver Haagh <oliver@dmc.international>
2023-04-27 10:59:42 +0200

    Extract creating libp2p to a separate method Also make the main context cancelable

--- router/router.go
+++ router/router.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p/core/host"
 	peerstore "github.com/libp2p/go-libp2p/core/peer"
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
 	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
@@ -31,8 +32,49 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	node, rd := setupLibP2P(ctx)
+
+	// Look for others who have announced and attempt to connect to them
+	anyConnected := false
+	for !anyConnected {
+		fmt.Println("Searching for peers...")
+		peerChan, err := rd.FindPeers(ctx, "over here")
+		if err != nil {
+			panic(err)
+		}
+		for p := range peerChan {
+			if p.ID == node.ID() {
+				continue // No self connection
+			}
+			fmt.Println("Peer:", p)
+			err := node.Connect(ctx, p)
+			if err != nil {
+				fmt.Println("Failed connecting to ", p.ID.String(), ", error:", err)
+			} else {
+				fmt.Println("Connected to:", p.ID.String())
+				anyConnected = true
+			}
+		}
+		time.Sleep(2 * time.Second)
+	}
+	fmt.Println("Peer discovery complete")
+
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+
+	// shut the node down
+	if err := node.Close(); err != nil {
+		fmt.Println("libp2p node could not be shut down correctly")
+	}
+}
+
+func setupLibP2P(ctx context.Context) (host.Host, *drouting.RoutingDiscovery) {
 	// start a libp2p node with default settings
 	node, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/udp/0/quic-v1", "/ip6/::/udp/0/quic-v1"))
 	if err != nil {
@@ -76,40 +118,5 @@ func main() {
 	}
 	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
 	fmt.Println("libp2p node addresses:", addrs)
-
-	// Look for others who have announced and attempt to connect to them
-	anyConnected := false
-	for !anyConnected {
-		fmt.Println("Searching for peers...")
-		peerChan, err := rd.FindPeers(ctx, "over here")
-		if err != nil {
-			panic(err)
-		}
-		for p := range peerChan {
-			if p.ID == node.ID() {
-				continue // No self connection
-			}
-			fmt.Println("Peer:", p)
-			err := node.Connect(ctx, p)
-			if err != nil {
-				fmt.Println("Failed connecting to ", p.ID.String(), ", error:", err)
-			} else {
-				fmt.Println("Connected to:", p.ID.String())
-				anyConnected = true
-			}
-		}
-		time.Sleep(2 * time.Second)
-	}
-	fmt.Println("Peer discovery complete")
-
-	// wait for a SIGINT or SIGTERM signal
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt)
-	<-ch
-	fmt.Println("Received signal, shutting down...")
-
-	// shut the node down
-	if err := node.Close(); err != nil {
-		panic(err)
-	}
+	return node, rd
 }
