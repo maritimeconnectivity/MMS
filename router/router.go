b56b3734831c3fa54d7255bba5838c88e8236297
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2023-11-27 14:13:53 +0100

    Extract methods for checking OCSP and CRL in Router

--- router/router.go
+++ router/router.go
@@ -732,65 +732,14 @@ func verifyEdgeRouterCertificate() func(rawCerts [][]byte, verifiedChains [][]*x
 
 		httpClient := http.DefaultClient
 		if len(clientCert.OCSPServer) > 0 {
-			ocspUrl := clientCert.OCSPServer[0]
-			ocspReq, err := ocsp.CreateRequest(clientCert, issuingCert, nil)
+			err := performOCSPCheck(clientCert, issuingCert, httpClient)
 			if err != nil {
-				return fmt.Errorf("could not create OCSP request for the given client cert: %w", err)
-			}
-			resp, err := httpClient.Post(ocspUrl, "application/ocsp-request", bytes.NewBuffer(ocspReq))
-			if err != nil {
-				return fmt.Errorf("could not send OCSP request: %w", err)
-			}
-			respBytes, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return fmt.Errorf("getting OCSP response failed: %w", err)
-			}
-			if err = resp.Body.Close(); err != nil {
-				return fmt.Errorf("could not close response body: %w", err)
-			}
-			ocspResp, err := ocsp.ParseResponse(respBytes, nil)
-			if err != nil {
-				return fmt.Errorf("parsing OCSP response failed: %w", err)
-			}
-			if ocspResp.SerialNumber.Cmp(clientCert.SerialNumber) != 0 {
-				return fmt.Errorf("the serial number in the OCSP response does not correspond to the serial number of the certificate being checked")
-			}
-			if ocspResp.Certificate == nil {
-				if err = ocspResp.CheckSignatureFrom(issuingCert); err != nil {
-					return fmt.Errorf("the signature on the OCSP response is not valid: %w", err)
-				}
-			}
-			if !ocspResp.Certificate.Equal(issuingCert) {
-				return fmt.Errorf("the certificate embedded in the OCSP response does not match the configured issuing CA")
-			}
-			if ocspResp.Status != ocsp.Good {
-				return fmt.Errorf("the given client certificate has been revoked")
+				return err
 			}
 		} else if len(clientCert.CRLDistributionPoints) > 0 {
-			crlURL := clientCert.CRLDistributionPoints[0]
-			resp, err := httpClient.Get(crlURL)
+			err := performCRLCheck(clientCert, httpClient, issuingCert)
 			if err != nil {
-				return fmt.Errorf("could not send CRL request: %w", err)
-			}
-			respBody, err := io.ReadAll(resp.Body)
-			if err != nil {
-				return fmt.Errorf("getting CRL response body failed: %w", err)
-			}
-			if err = resp.Body.Close(); err != nil {
-				return fmt.Errorf("failed to close CRL response: %w body", err)
-			}
-			crl, err := x509.ParseRevocationList(respBody)
-			if err != nil {
-				return fmt.Errorf("could not parse received CRL: %w", err)
-			}
-			if err = crl.CheckSignatureFrom(issuingCert); err != nil {
-				return fmt.Errorf("signature on CRL is not valid: %w", err)
-			}
-			now := time.Now().UTC()
-			for _, rev := range crl.RevokedCertificateEntries {
-				if (rev.SerialNumber.Cmp(clientCert.SerialNumber) == 0) && (rev.RevocationTime.UTC().Before(now)) {
-					return fmt.Errorf("the given client certificate has been revoked")
-				}
+				return err
 			}
 		} else {
 			return fmt.Errorf("was not able to check revocation status of client certificate")
@@ -798,6 +747,73 @@ func verifyEdgeRouterCertificate() func(rawCerts [][]byte, verifiedChains [][]*x
 
 		return nil
 	}
+}
+
+func performOCSPCheck(clientCert *x509.Certificate, issuingCert *x509.Certificate, httpClient *http.Client) error {
+	ocspUrl := clientCert.OCSPServer[0]
+	ocspReq, err := ocsp.CreateRequest(clientCert, issuingCert, nil)
+	if err != nil {
+		return fmt.Errorf("could not create OCSP request for the given client cert: %w", err)
+	}
+	resp, err := httpClient.Post(ocspUrl, "application/ocsp-request", bytes.NewBuffer(ocspReq))
+	if err != nil {
+		return fmt.Errorf("could not send OCSP request: %w", err)
+	}
+	respBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("getting OCSP response failed: %w", err)
+	}
+	if err = resp.Body.Close(); err != nil {
+		return fmt.Errorf("could not close response body: %w", err)
+	}
+	ocspResp, err := ocsp.ParseResponse(respBytes, nil)
+	if err != nil {
+		return fmt.Errorf("parsing OCSP response failed: %w", err)
+	}
+	if ocspResp.SerialNumber.Cmp(clientCert.SerialNumber) != 0 {
+		return fmt.Errorf("the serial number in the OCSP response does not correspond to the serial number of the certificate being checked")
+	}
+	if ocspResp.Certificate == nil {
+		if err = ocspResp.CheckSignatureFrom(issuingCert); err != nil {
+			return fmt.Errorf("the signature on the OCSP response is not valid: %w", err)
+		}
+	}
+	if !ocspResp.Certificate.Equal(issuingCert) {
+		return fmt.Errorf("the certificate embedded in the OCSP response does not match the configured issuing CA")
+	}
+	if ocspResp.Status != ocsp.Good {
+		return fmt.Errorf("the given client certificate has been revoked")
+	}
+	return nil
+}
+
+func performCRLCheck(clientCert *x509.Certificate, httpClient *http.Client, issuingCert *x509.Certificate) error {
+	crlURL := clientCert.CRLDistributionPoints[0]
+	resp, err := httpClient.Get(crlURL)
+	if err != nil {
+		return fmt.Errorf("could not send CRL request: %w", err)
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("getting CRL response body failed: %w", err)
+	}
+	if err = resp.Body.Close(); err != nil {
+		return fmt.Errorf("failed to close CRL response: %w body", err)
+	}
+	crl, err := x509.ParseRevocationList(respBody)
+	if err != nil {
+		return fmt.Errorf("could not parse received CRL: %w", err)
+	}
+	if err = crl.CheckSignatureFrom(issuingCert); err != nil {
+		return fmt.Errorf("signature on CRL is not valid: %w", err)
+	}
+	now := time.Now().UTC()
+	for _, rev := range crl.RevokedCertificateEntries {
+		if (rev.SerialNumber.Cmp(clientCert.SerialNumber) == 0) && (rev.RevocationTime.UTC().Before(now)) {
+			return fmt.Errorf("the given client certificate has been revoked")
+		}
+	}
+	return nil
 }
 
 func handleSubscription(ctx context.Context, sub *pubsub.Subscription, host *host.Host, incomingChannel chan<- *mmtp.MmtpMessage, wg *sync.WaitGroup) {
