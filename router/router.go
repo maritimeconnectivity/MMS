f8d65eb1a32fb09af2aebe43ac064b7c1b62f0ad
jakobsve <jakob.svenningsen@outlook.com>
2024-08-09 13:26:09 +0200

    Disable WS Read limit

--- router/router.go
+++ router/router.go
@@ -56,7 +56,7 @@ import (
 )
 
 const (
-	WsReadLimit      int64         = 1 << 20             // 1 MiB = 1048576 B
+	WsReadLimit      int64         = -1                  // 1 MiB = 1048576 B
 	MessageSizeLimit int           = 50 * (1 << 10)      // 50 KiB = 51200 B
 	ExpirationLimit  time.Duration = time.Hour * 24 * 30 // 30 days
 	ChannelBufSize   int           = 1048576
